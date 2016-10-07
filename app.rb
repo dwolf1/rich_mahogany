7d6b5b16394ee042e409864258cdd6a500e37d8b
dwolf1 <dannawolf1@gmail.com>
2016-10-07 14:30:39 -0400

    added navbar

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
  require 'sinatra'
 
  get '/' do 
+ 	@menu = [
+	    {page: 'Home', href: '/'},
+	    {page: 'About', href: '/about'},
+	    {page: 'Gallery', href: '/gallery'},
+	    {page: 'Schedule', href: '/schedule'},
+	    {page: 'News', href: '/news'},
+	    {page: 'Contact', href: '/contact'}
+  	]
+
  	erb :home
  end
